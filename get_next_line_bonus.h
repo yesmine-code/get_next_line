5f21891cd14e1dc185ed8a61a5a3af78175695e5
ybesbes <ybesbes@student.42.fr>
2020-07-12 19:53:53 +0200

    my get next line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 21:13:23 by ybesbes           #+#    #+#             */
+/*   Updated: 2020/06/21 21:13:40 by ybesbes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, char *src);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		get_next_line(int fd, char **line);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybesbes <ybesbes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/02 12:12:16 by ybesbes           #+#    #+#             */
+/*   Updated: 2020/06/21 21:15:14 by ybesbes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	const char	*ptr;
+	size_t		i;
+
+	ptr = str;
+	i = 0;
+	while (ptr[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(*str) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str != NULL)
+	{
+		while (i < ft_strlen(s1))
+		{
+			str[i] = s1[i];
+			i++;
+		}
+		while (j < ft_strlen(s2))
+		{
+			str[i] = s2[j];
+			i++;
+			j++;
+		}
+		str[i] = '\0';
+	}
+	return (str);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(*dest) * (ft_strlen(s) + 1));
+	i = 0;
+	if (dest != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			dest[i] = s[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if ((char)c == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char*)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	i = 0;
+	ptr = malloc(sizeof(*ptr) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
